043efa5635eeb960d7ca6b40bd8d6e4cdbdcb641
LucasFigueiredoDEV <lucassamuell.dev@gmail.com>
2023-06-03 22:15:21 -0300

    Adicionando arquivos de aulas concluidas

---
+++ aula15.py
@@ -0,0 +1,5 @@
+# copy - retorna uma cópia rasa (shallow copy)
+# get - obtém uma chave
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro
