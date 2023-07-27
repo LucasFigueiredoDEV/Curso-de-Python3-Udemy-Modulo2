9028163373f2da9dd73ca2a465e1e1e0c9ebc490
LucasFigueiredoDEV <lucassamuell.dev@gmail.com>
2023-07-26 22:09:41 -0300

    Adicionando arquivos de aulas concluidas

---
+++ aula62.py
@@ -0,0 +1,10 @@
+# Controlando a quantidade de argumentos posicionais e nomeados em funções
+# *args (ilimitado de argumentos posicionais)
+# **kwargs (ilimitado de argumentos nomedos)
+# Positional-only Parameters (/) - Tudo antes da barra deve
+# ser APENAS posicional.
+# PEP 570 - Python Positional-Only Parameters
+# https://peps.python.org/pep-0570/
+# Keyword-Only Argments (*) - * sozinho NÃO SUGA valores.
+# PEP 3102 - Keyword-Only Arguments
+# https://peps.python.org/pep-3102/
