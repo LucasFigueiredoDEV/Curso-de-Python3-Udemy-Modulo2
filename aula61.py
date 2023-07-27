065ca1186b26ffd2bc50cb5bb562ea8d070222bf
LucasFigueiredoDEV <lucassamuell.dev@gmail.com>
2023-07-26 21:44:28 -0300

    Adicionando arquivos de aulas concluidsa

--- aula61.py
+++ aula61.py
@@ -8,6 +8,7 @@
 # desfazer = [] -> Refazer ['Caminhar', 'Fazer café']
 # Refazer = todo ['Fazer café']
 # Refazer = todo ['Fazer café', 'Caminhar']
+import json
 import os
 
 
@@ -33,6 +34,7 @@ def desfazer(tarefas, tarefas_refazer):
     print(f'{tarefa=} removida da lista de tarefas.')
     tarefas_refazer.append(tarefa)
     print()
+    listar(tarefas)
 
 
 def refazer(tarefas, tarefas_refazer):
@@ -45,6 +47,7 @@ def refazer(tarefas, tarefas_refazer):
     print(f'{tarefa=} adicionada na lista de tarefas.')
     tarefas.append(tarefa)
     print()
+    listar(tarefas)
 
 
 def adicionar(tarefa, tarefas):
@@ -56,30 +59,43 @@ def adicionar(tarefa, tarefas):
     print(f'{tarefa=} adicionada na lista de tarefas.')
     tarefas.append(tarefa)
     print()
+    listar(tarefas)
 
 
-tarefas = []
+def ler(tarefas, caminho_arquivo):
+    dados = []
+    try:
+        with open(caminho_arquivo, 'r', encoding='utf8') as arquivo:
+            dados = json.load(arquivo)
+    except FileNotFoundError:
+        print('Criando arquivo')
+        salvar(tarefas, caminho_arquivo)
+    return dados
+
+
+def salvar(tarefas, caminho_arquivo):
+    dados = tarefas
+    with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
+        dados = json.dump(tarefas, arquivo, indent=2, ensure_ascii=False)
+    return dados
+
+
+CAMINHO_ARQUIVO = 'aula61.json'
+tarefas = ler([], CAMINHO_ARQUIVO)
 tarefas_refazer = []
 
 while True:
     print('Comandos: listar, desfazer e refazer')
     tarefa = input('Digite uma tarefa ou comando: ')
 
-    if tarefa == 'listar':
-        listar(tarefas)
-        continue
-    elif tarefa == 'desfazer':
-        desfazer(tarefas, tarefas_refazer)
-        listar(tarefas)
-        continue
-    elif tarefa == 'refazer':
-        refazer(tarefas, tarefas_refazer)
-        listar(tarefas)
-        continue
-    elif tarefa == 'clear':
-        os.system('clear')
-        continue
-    else:
-        adicionar(tarefa, tarefas)
-        listar(tarefas)
-        continue
+    comandos = {
+        'listar': lambda: listar(tarefas),
+        'desfazer': lambda: desfazer(tarefas, tarefas_refazer),
+        'refazer': lambda: refazer(tarefas, tarefas_refazer),
+        'clear': lambda: os.system('clear'),
+        'adicionar': lambda: adicionar(tarefa, tarefas),
+    }
+    comando = comandos.get(tarefa) if comandos.get(tarefa) is not None else \
+        comandos['adicionar']
+    comando()
+    salvar(tarefas, CAMINHO_ARQUIVO)
